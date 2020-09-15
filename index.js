d8bc5bc29849beeadd7ed7c298fc44b694d95b9e
Ahmed Al-Majidi <af774857999@gmail.com>
2020-09-15 13:55:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,17 +4,6 @@ function testFunc() {
   return "hi"
 }
 
-// const superbowlWin = (arr) => {
-//   const fn = () => {
-//     for (element of arr){
-//     if (x.result === "W") {
-//       console.log("W");
-//     } else {}
-//   }
-//   }
-//
-// }
-
 const superbowlWin = (arr) => {
    let x = arr.find(ele => ele.result === "W");
    if (x) {
